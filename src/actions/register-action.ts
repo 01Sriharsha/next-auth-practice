8583d34ee4b9ee5f836027d26fe556bc3b240ba2
01Sriharsha <harshavasu463@gmail.com>
2024-02-15 20:45:10 +0530

    added db connection logic

--- src/actions/register-action.ts
+++ src/actions/register-action.ts
@@ -1,12 +1,37 @@
 "use server";
 
+import { hash } from "bcryptjs";
 import { RegisterSchema, RegisterSchemaType } from "@/schemas";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/util/user";
 
 export const register = async (values: RegisterSchemaType) => {
   const parsedData = RegisterSchema.safeParse(values);
 
   if (!parsedData.success) {
-    return { error: "Invalid fields" };
+    throw new Error("Invalid fields");
+  }
+
+  const { email, password, name } = parsedData.data;
+
+  const userExists = await getUserByEmail(email);
+
+  if (userExists) {
+    throw new Error("User already exists");
+  }
+
+  const hashedPassword = await hash(password, 10);
+
+  const user = await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  if (!user) {
+    throw new Error("Failed to create user");
   }
 
   return { success: "Email sent" };
