60b5a41f0e256c8945db79290e8cce8e9486c21f
01Sriharsha <harshavasu463@gmail.com>
2024-02-15 20:46:41 +0530

    Added next-auth's credentials provider

--- src/actions/login-action.ts
+++ src/actions/login-action.ts
@@ -1,13 +1,32 @@
 "use server";
 
+import { AuthError } from "next-auth";
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { LoginSchema, LoginSchemaType } from "@/schemas";
 
 export const login = async (values: LoginSchemaType) => {
   const parsedData = LoginSchema.safeParse(values);
 
   if (!parsedData.success) {
-    return { error: "Invalid fields" };
+    throw new Error("Invalid fields");
   }
 
-  return { success: "Email sent" };
+  const { email, password } = parsedData.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+    return { success: "Login successfull!" };
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          throw new Error("Invalid credentials");
+      }
+    }
+  }
 };
--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,7 +1,27 @@
-import GitHub from "next-auth/providers/github"
-
-import type { NextAuthConfig } from "next-auth"
+import Credentials from "next-auth/providers/credentials";
+import Github from "next-auth/providers/github";
+import type { NextAuthConfig } from "next-auth";
+import bcryptjs from "bcryptjs";
+import { LoginSchema } from "@/schemas";
+import { getUserByEmail } from "@/util/user";
 
 export default {
-  providers: [GitHub],
-} satisfies NextAuthConfig
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validateCredentials = LoginSchema.safeParse(credentials);
+        if (validateCredentials.success) {
+          const { email, password } = validateCredentials.data;
+
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
+
+          const passwordMatch = await bcryptjs.compare(password, user.password);
+
+          if (passwordMatch) return user;
+        }
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
